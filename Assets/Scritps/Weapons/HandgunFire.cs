d0ac5bf1e08aa913998ae76cb1f318a208ead785
Kuba <jakub.olszak2@gmail.com>
2020-01-14 06:27:51 +0100

    add first level

---
+++ Assets/Scritps/Weapons/HandgunFire.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandgunFire : MonoBehaviour
+{
+    public GameObject theGun;
+    public GameObject muzzleFlash;
+    public AudioSource gunFire;
+    public bool isFiring = false;
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if (isFiring == false)
+            {
+                StartCoroutine(FiringHandgun());
+            }
+        }
+    }
+
+    IEnumerator FiringHandgun()
+    {
+        isFiring = true;
+        theGun.GetComponent<Animator>().Play("HandgunFire");
+        muzzleFlash.SetActive(true);
+        gunFire.Play();
+        yield return new WaitForSeconds(0.05f);
+        muzzleFlash.SetActive(false);
+        yield return new WaitForSeconds(0.2f);
+        theGun.GetComponent<Animator>().Play("New State");
+        isFiring = false;
+    }
+}
+
